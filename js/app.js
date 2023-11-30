ac8b71742b293957d5d8e405b861e99520bf525b
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-11-30 09:32:20 +0600

    indexing

--- js/app.js
+++ js/app.js
@@ -31,22 +31,22 @@
         noPhone.classList.add('d-none')
     }
 
-    // display all phone 
-    phones.forEach(phone => {
+    phones.forEach((phone, index) => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
-        <div class="card p-3">
-        <img src="${phone.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${phone.phone_name}</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
-        </div>
+            <div class="card p-3">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${index + 1}. ${phone.phone_name}</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
         `;
         // element inside the card 
-        phonesContainer.appendChild(phoneDiv)
+        phonesContainer.appendChild(phoneDiv);
     });
+    
     // stop loader 
     toggleSpinner(false)
 }
